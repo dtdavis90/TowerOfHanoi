c6d3d64435c9866e3159391ebd4e7f36c7b34c18
ddtdavis90@gmail.com <dtdavis90@gmail.com>
2020-06-28 20:30:58 -0400

    pushing from mac -> pc sunday night

---
+++ hello.py
@@ -0,0 +1,34 @@
+
+class puck:
+
+    previous_spire
+    def __init__ (current_spire, has_moved):
+        self.current_spire = current_spire
+        self.has_moved = has_moved
+
+    def set_current_spire(spire):
+        self.current_spire = spire
+
+    def set_previous_spire(spire):
+        self.previous_spire = spire
+
+
+class spire_controller:
+
+    spire1 = []
+    spire2 = []
+    spire3 = []
+
+    def create_pucks(num_of_pucks);
+        
+        for x in range(0,num_of_pucks)
+            
+
+    def move():
+        
+    def legal_move_check():
+
+    
+
+def main():
+    num_of_pucks = 3
